6a80015eb443d446eaf89915de97ac4c967486dd
mykelordtogi <78527834+mykelordtogi@users.noreply.github.com>
2021-02-14 13:38:49 +0800

    my activity

---
+++ prelim_exer1.php
@@ -0,0 +1,17 @@
+<html>
+<head> <title>PHP Tutorial</title></head>
+
+<body>
+
+    <?php
+
+    
+
+    echo "PHP, an acronym for Hypertext Preprocessor, is a widely-used open source general-purpose scripting language. It is a cross-platform, HTML embedded server-side scripting language and is especially suited for web development.";
+  
+
+    ?>
+<a href="mylink.php">Go to my link</a>
+
+
+</body>
