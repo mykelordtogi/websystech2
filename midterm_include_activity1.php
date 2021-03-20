fc0f7673309cd39f21f95efd93983209565fe29d
mykelordtogi <78527834+mykelordtogi@users.noreply.github.com>
2021-03-20 17:03:01 +0800

    new updated

--- midterm_include_activity1.php
+++ midterm_include_activity1.php
@@ -74,11 +74,73 @@ echo "I am an ordinary car.";
 
 }
 
+function mycar3()
+{
+
+$mynewcar = new Car();
+
+$mynewcar->setMyMake("Chevy");
+($mynewcar->setMySpeed("170"));
+
+$newspeed=intval($mynewcar->getMyMaxSpeed());
+
+
+if($newspeed > 200)
+{
+
+echo "Beep! I am a ".$mynewcar->getMymake()."<br>";
+echo "I am a sports car. I can run at maximum speed of ".$newspeed;
+
+}
+
+
+if($newspeed <= 140)
+{
+echo "Beep! I am a ".$mynewcar->getMymake()."<br>";
+echo "I am an ordinary car.";
+}
+
+}
+
+function mycar4()
+{
+
+$mynewcar = new Car();
+
+$mynewcar->setMyMake("Jeep");
+($mynewcar->setMySpeed("50"));
+
+$newspeed=intval($mynewcar->getMyMaxSpeed());
+
+
+if($newspeed > 200)
+{
+
+echo "Beep! I am a ".$mynewcar->getMymake()."<br>";
+echo "I am a sports car. I can run at maximum speed of ".$newspeed;
+
+}
+
+
+if($newspeed <= 140)
+{
+echo "Beep! I am a ".$mynewcar->getMymake()."<br>";
+echo "I am an ordinary car.";
+}
+
+}
 
 mycar1();
 echo "<br>";
 echo "<br>";
 mycar2();
+echo "<br>";
+echo "<br>";
+mycar3();
+echo "<br>";
+echo "<br>";
+mycar4();
+
 
 
 
