4f4e3face3394e42e6d7fa1a7017eb639bf432af
mykelordtogi <78527834+mykelordtogi@users.noreply.github.com>
2021-05-10 14:56:06 +0800

    Add files via upload

---
+++ myproj2/pages/error.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+
+<body>
+
+<div class="container">
+	<br><br>
+  <div class="alert alert-danger">
+    <strong>Invalid Credentials!</strong><br>Make sure if Email or Password is correct or Registered.
+
+    <br>
+    <br>
+    <a href="login.php">Try logging in.</a><br>
+    <a href="register.php">Make an Account.</a>
+
+  </div>
+</div>
+
+
+</body>
+</html>
